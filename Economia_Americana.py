fde0d91ead6db1b3897ee15f49b8eef4962c7855
marianegriel <150819818+marianegriel@users.noreply.github.com>
2024-01-18 16:36:37 -0300

    Create Economia_Americana.py

---
+++ Economia_Americana.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+#### CONFIGURAÇÕES GERAIS ####  
+st.set_page_config(
+    page_title="Observatório - Rohden",
+    layout="wide",
+    page_icon = "https://observatorio.fiesc.com.br/sites/default/files/2021-10/OOBSERVATORIOFIESC_MARCA.png"
+)
